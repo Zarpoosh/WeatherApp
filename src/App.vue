8e1fdb962852faa3ee5f11b249199ee3640e6127
Zarpoosh <mzar67839@gmail.com>
2024-09-03 19:49:33 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SideNavigation />
+    <RouterView />
+  </div>
+  <Home />
+</template>
+
+<script>
+import { RouterView } from "vue-router";
+import SideNavigation from "./components/SideNavigation.vue";
+export default {
+  components: { SideNavigation },
+};
+</script>
+
+<style></style>
---
+++ src/components/BaseModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md"
+          >
+            <slot />
+            <button
+              @click="$emit('close-modal')"
+              class="text-white mt-8 bg-weather-primary py-2 px-6"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<script setup>
+defineEmits(["close-modal"]);
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
