90be4662b5bf8ce2b27d7ef9d5d1c0b3be5fc6fa
Zarpoosh <mzar67839@gmail.com>
2024-09-03 20:23:24 +0330

    add Search input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1 class="text-red-500">hello world :)))</h1>
+  <main class="container">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text "
+        placeholder="dearch for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondry focus:outline-none focus:[0px-1px-0-0-#004E71]"
+      />
+    </div>
+  </main>
 </template>
 
 <script>
